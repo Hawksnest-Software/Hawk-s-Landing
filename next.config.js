49e76b82e9ec31cf340a2696b9d84c6c27ea4730
burningserenity <burningserenity@novo-ordo.com>
2025-10-31 09:38:50 -0400

    CI

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/Hawk-s-Landing',
+  assetPrefix: '/Hawk-s-Landing',
   trailingSlash: true,
   images: {
     unoptimized: true
